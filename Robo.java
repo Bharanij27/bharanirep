31812df58627cd392dedd13f01e4936b72659a74
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-08-23 09:26:39 +0530

    Update Robo.java

--- Robo.java
+++ Robo.java
@@ -1,6 +1,6 @@
 class Robo{
 public static void main(String args[0]){
-String a[]=input3.split("-");
+	String a[]=input3.split("-");
 		int init1=Integer.parseInt(a[0]);
 		int init2=Integer.parseInt(a[1]);
 		String mm=a[2];
@@ -8,9 +8,16 @@ String a[]=input3.split("-");
 		input4=input4.replaceAll(" ","");
 		char dir[]=input4.toCharArray();
 		int i=0;
-		while (i!=input4.length())
-		{  	char ax=' ';
-			char ay=' ';
+		char ax=' ';
+		char ay=' ';
+		if(dir[i]=='M'){
+			if(d=='N'){ay='d';}
+			else if(d=='S'){ay='i';}
+			else if(d=='E'){ax='i';}
+			else {ax='d';}
+		}
+		while (i<input4.length())
+		{  	
 			if (dir[i]!='M'){
 			if ((d=='E' && dir[i]=='R') || (d=='W' && dir[i]=='L')) {
 				d='S';
