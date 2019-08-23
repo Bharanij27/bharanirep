cab26d31c38ffad87182c4df07c377bb2d93bbff
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-08-23 01:42:52 -0700

    Update Arr3swap.py

--- Arr3swap.py
+++ Arr3swap.py
@@ -1,5 +1,5 @@
 k=int(input())
 n=list(map(int,input().split()))
-for i in range(0,k//2):
-    n[i],n[i+1]=n[i+1],n[i]
+for i in range(0,k,2):
+    if k%2!=0 and i!=k-1: n[i],n[i+1]=n[i+1],n[i]
 print(*n)
